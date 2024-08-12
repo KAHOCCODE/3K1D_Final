3e7cb32c920f1374b2d505b315ea4a15254fb4eb
P1emon <khang113324@gmail.com>
2024-08-13 02:30:02 +0700

    Làm giao diện cx như là chức năng cơ bản cho customer

---
+++ 3K1D_Final/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3K1D_Final.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
