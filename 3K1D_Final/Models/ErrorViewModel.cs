8f1732df2834fe2708af8b0ed00901fe581d50f3
ka do doka <hoangkavudo@gmail.com>
2024-08-12 13:46:44 +0700

    Add project files.

---
+++ 3K1D_Final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _3K1D_Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
