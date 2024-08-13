49c0494c981f1142cd3403f84798d572405e95b6
P1emon <khang113324@gmail.com>
2024-08-14 00:22:43 +0700

    Sửa lại giao diện và thêm create được khách hàng

--- 3K1D_Final/Areas/Admin/Controllers/HomeAdminController.cs
+++ 3K1D_Final/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,13 +1,86 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _3K1D_Final.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace _3K1D_Final.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class HomeAdminController : Controller
     {
-        [Area("Admin")]
+        QlrapPhimContext db = new QlrapPhimContext();
+
+        [Route("Admin/HomeAdmin")]
+        [Route("Admin/")]
+        [Route("Index")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("Admin/Customer/CustomerLists")]
+        public IActionResult CustomerLists()
+        {
+            var CUSTOMERS = db.KhachHangs.ToList();
+            return View(CUSTOMERS);
+        }
+
+        [Route("Admin/Customer/Create")]
+        [HttpGet]
+        public IActionResult CustomerCreate()
+        {
+            return View();
+        }
+
+        [Route("Admin/Customer/Create")]
+        [HttpPost]
+        public IActionResult CustomerCreate(KhachHang kh)
+        {
+            if (ModelState.IsValid)
+            {
+                db.KhachHangs.Add(kh);
+                db.SaveChanges();
+                return RedirectToAction("CustomerLists");
+            }
+            return View(kh);
+        }
+
+        [Route("Admin/Customer/Edit/{id}")]
+        [HttpGet]
+        public IActionResult CustomerEdit(string id)
+        {
+            var customer = db.KhachHangs.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+
+        [Route("Admin/Customer/Edit/{id}")]
+        [HttpPost]
+        public IActionResult CustomerEdit(string id, KhachHang updatedCustomer)
+        {
+            var customer = db.KhachHangs.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                customer.HoTen = updatedCustomer.HoTen;
+                customer.NgaySinh = updatedCustomer.NgaySinh;
+                customer.DiaChi = updatedCustomer.DiaChi;
+                customer.Sdt = updatedCustomer.Sdt;
+                customer.Cccd = updatedCustomer.Cccd;
+                // Update other properties as needed
+
+                db.SaveChanges();
+                return RedirectToAction("CustomerLists");
+            }
+
+            return View(updatedCustomer);
         }
     }
 }
--- 3K1D_Final/Models/KhachHang.cs
+++ 3K1D_Final/Models/KhachHang.cs
@@ -1,30 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace _3K1D_Final.Models;
 
-public partial class KhachHang
+public partial class KhachHang : IValidatableObject
 {
     [DisplayName("Mã khách hàng")]
+    [Required(ErrorMessage = "Mã khách hàng không được để trống")]
+    [RegularExpression(@"^KH\d+$", ErrorMessage = "Mã khách hàng phải bắt đầu bằng 'KH' và theo sau là số")]
     public string IdKhachHang { get; set; } = null!;
+
     [DisplayName("Họ tên")]
+    [Required(ErrorMessage = "Họ tên không được để trống")]
+    [MaxLength(50, ErrorMessage = "Họ tên không được vượt quá 50 ký tự")]
     public string HoTen { get; set; } = null!;
+
     [DisplayName("Ngày sinh")]
-
+    [Required(ErrorMessage = "Ngày sinh không được để trống")]
     public DateOnly NgaySinh { get; set; }
+
     [DisplayName("Địa chỉ")]
+    [Required(ErrorMessage = "Địa chỉ không được để trống")]
+    [MaxLength(100, ErrorMessage = "Địa chỉ không được vượt quá 100 ký tự")]
+    public string DiaChi { get; set; } = null!;
 
-    public string? DiaChi { get; set; }
     [DisplayName("Số điện thoại")]
+    [Required(ErrorMessage = "Số điện thoại không được để trống")]
+    [MaxLength(10, ErrorMessage = "Số điện thoại phải là 10 số")]
+    [MinLength(10, ErrorMessage = "Số điện thoại phải là 10 số")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "Số điện thoại chỉ được chứa 10 chữ số")]
+    public string Sdt { get; set; } = null!;
 
-    public string? Sdt { get; set; }
     [DisplayName("CCCD")]
+    [Required(ErrorMessage = "CCCD không được để trống")]
+    [MaxLength(12, ErrorMessage = "CCCD phải là 12 số")]
+    [MinLength(12, ErrorMessage = "CCCD phải là 12 số")]
+    [RegularExpression(@"^\d{12}$", ErrorMessage = "CCCD chỉ được chứa 12 chữ số")]
+    public string Cccd { get; set; } = null!;
 
-    public string? Cccd { get; set; }
     [DisplayName("Điểm tích lũy")]
-
     public int? DiemTichLuy { get; set; }
 
     public virtual ICollection<Ve> Ves { get; set; } = new List<Ve>();
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!Regex.IsMatch(IdKhachHang, @"^KH\d+$"))
+        {
+            yield return new ValidationResult("Mã khách hàng phải bắt đầu bằng 'KH' và theo sau là số", new[] { nameof(IdKhachHang) });
+        }
+    }
 }
