636ec1f5afe8f9e3f23a7d65c4d7ee22a90d5a72
Pkmax-bit <2100010779@nttu.edu.vn>
2024-08-14 22:30:37 +0700

    lấy được dữ liệu phim từ database cho details ( 1/2) thiếu ảnh và thể loại

---
+++ 3K1D_Final/Controllers/MovieDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace _3K1D_Final.Controllers
+{
+    internal class MovieDetailsViewModel
+    {
+        private object movie;
+
+        public MovieDetailsViewModel(object movie)
+        {
+            this.movie = movie;
+        }
+    }
+}
